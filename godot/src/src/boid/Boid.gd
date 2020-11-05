efdef8300008b9248d037623488b56f154ea7aad
Scott O'Malley <scottyomalley@gmail.com>
2020-11-05 21:35:50 +0000

    Boids now move straight

--- godot/src/src/boid/Boid.gd
+++ godot/src/src/boid/Boid.gd
@@ -1,23 +1,27 @@
 extends KinematicBody2D
-var random = RandomNumberGenerator.new()
 var _speed = 100
+var _turn_speed = 100
 var _target = Vector2()
 var _velocity = Vector2()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	random.randomize()
+	randomize()
 	var _screen_size = get_viewport_rect().size
-	var x = random.randf_range(10, _screen_size.x - 10)
-	var y = random.randf_range(10, _screen_size.y - 10)
+	var x = rand_range(10, _screen_size.x - 10)
+	var y = rand_range(10, _screen_size.y - 10)
+	rotation += rand_range(-PI, PI)
 	position = Vector2(x, y)
-	_velocity = position
+	_velocity = Vector2.ZERO
 	
 func _process(delta: float) -> void:
-	_target = get_global_mouse_position()
-	if position.distance_to(_target) > 10:
-		look_at(get_global_mouse_position())
-		_velocity = (_target - position).normalized() * _speed
-	else:
-		_velocity = Vector2()
-	pass
+	
+#	_target = get_global_mouse_position()
+#	if position.distance_to(_target) > 10:
+#		look_at(get_global_mouse_position())
+#		_velocity = (_target - position).normalized() * _speed
+#	else:
+#		_velocity = Vector2()
+#	pass
+#	position += _velocity * delta
+	_velocity = Vector2(_speed, 0).rotated(rotation)
 	position += _velocity * delta
