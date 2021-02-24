d91ac60b0e657d7b619c630ab2310562d1bf1f08
Scott O'Malley <scottyomalley@gmail.com>
2021-02-24 10:30:58 +0000

    The screen now wraps, moar boids

--- godot/src/boid/Boid.gd
+++ godot/src/boid/Boid.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
-var _speed = 100
+const SPEED = 100
+const COLLISION_RADIUS = 50
 var _turn_speed = 100
 var _target = Vector2()
-var _velocity = Vector2()
+var velocity = Vector2()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
@@ -10,68 +11,79 @@ func _ready():
 	var x = rand_range(10, _screen_size.x - 10)
 	var y = rand_range(10, _screen_size.y - 10)
 	rotation += rand_range(-PI, PI)
-	position = Vector2(x, y)
-	_velocity = Vector2.ZERO
+	position = Vector2(x, y)	
 	
 func _process(delta: float) -> void:	
-	position = _updatePosition(position, _velocity, delta);
+	position = _calcAcceleration(delta);
+	position = wrapBorders(position)
 	
-
-func _updatePosition(position: Vector2, velocity: Vector2, delta: float) -> Vector2:
-	_velocity = Vector2.ZERO;
-	var coh = _calculateCohesion()
-	var sep = _calculateSeparation()
-	print(coh, sep)
-	_velocity = coh+sep
-	_velocity = (_velocity - position).normalized() * _speed
-
-	return position + _velocity * delta
-
-
-func _updatePositionToFollowMouse(position: Vector2, velocity: Vector2, rotation: float, delta: float) -> Vector2:
-	_target = get_global_mouse_position()
-	if position.distance_to(_target) > 10:
-		look_at(get_global_mouse_position())
-		_velocity = (_target - position).normalized() * _speed
-	else:
-		_velocity = Vector2()
-	pass
-	return position + _velocity * delta
-
-func _calculateCohesion() -> Vector2:
+func _calcAcceleration(delta: float) -> Vector2:
+	var flock = getFlock()
+	var coh = _calculateCohesion(flock)
+	var sep = _calculateSeparation(flock)
+	var ali = _calculateAlignment(flock)
+	velocity = (Vector2.ZERO+coh+sep+ali).normalized()		
+	look_at(velocity * 1000)
+#	velocity += Vector2(SPEED, 0).rotated(rotation)
+	return position + velocity * delta * SPEED
+	
+func wrapBorders(postion: Vector2) -> Vector2:
+	var _screen_size = get_viewport_rect().size
+	if(position.x > _screen_size.x):
+		position.x = 0
+	if(position.x < 0):
+		position.x = _screen_size.x
+	if(position.y > _screen_size.y):
+		position.y = 0
+	if(position.y < 0):
+		position.y = _screen_size.y
+	return position
+	
+func getFlock() -> Array:
 	var flock = self.get_parent().get_children()
-	var targetPos = Vector2.ZERO
+	var neighbors = []
+	
 	for boid in flock:
-		targetPos += boid.position
+		var dist = position.distance_to(boid.position)
+		if (boid != self && dist > 0 && dist <= COLLISION_RADIUS):
+			neighbors.append(boid)
+	return neighbors
+
+func _calculateCohesion(flock: Array) -> Vector2:
+	var coh = Vector2.ZERO
 	
-	return targetPos / flock.size()
+	if (flock.size() == 0):
+		return 	coh
+	
+	for boid in flock:
+		coh += boid.position
 
+	coh = coh / flock.size()
+	coh = coh - position
+	
+	return coh.normalized()
 
-# if ((d > 0) && (d < desiredseparation)) {
-#        // Calculate vector pointing away from neighbor
-#        PVector diff = PVector.sub(position, other.position);
-#        diff.normalize();
-#        diff.div(d);        // Weight by distance
-#        steer.add(diff);
-#        count++;            // Keep track of how many
-#      }
+func _calculateSeparation(flock: Array) -> Vector2:
+	var sep = Vector2.ZERO
+	
+	if (flock.size() == 0):
+		return 	sep
 
-func _calculateSeparation() -> Vector2:
-	var minDistance = 25.0;
-	var flock = self.get_parent().get_children()
-	var targetPos = Vector2.ZERO
 	var count = 0;
 	for boid in flock:
-		var distance = position.distance_to(boid.position)		
-		if(distance > 0 && distance < minDistance):
-			var differance: Vector2 = position - boid.position
-			differance = differance.normalized()
-			differance =  differance/distance
-			targetPos += differance
-			count += 1
+		var towardsMe: Vector2 = position - boid.position
+		if towardsMe.length() > 0:
+			sep += (towardsMe.normalized() / towardsMe.length())
+			
+	return sep.normalized()
 
-	if(count > 0):
-		 targetPos /= count;
+func _calculateAlignment(flock: Array) -> Vector2:
+	var ali = Vector2.ZERO
 	
-	return targetPos.normalized() * _speed
-		
+	if (flock.size() == 0):
+		return 	ali
+	
+	for boid in flock:
+		ali += boid.velocity
+			
+	return ali.normalized()
